372387fb43c9be1d7af34e61af924817e4bfbc61
Vinhdev2023 <daothanhvinh2004@gmail.com>
2023-08-08 16:22:26 +0700

    new update

--- admin/logout.php
+++ admin/logout.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-
+session_unset();
 unset($_SESSION['username']);
-unset($_SESSION['userpass']);
+unset($_SESSION['password']);
 
 // session_destroy();
 header('location:index.php');
