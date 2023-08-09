380375da9512ee85b0dac672d806c51f6b26a9ec
Vinhdev2023 <daothanhvinh2004@gmail.com>
2023-08-09 18:45:37 +0700

    vinh updated

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (isset($_POST['add_to_cart'])) {
         mysqli_query($conn, "INSERT INTO `tbl_orders`(customer_id, staff_id, prd_name, prd_price, prd_quantity, prd_image, cart_satus) VALUES('$user_id', '2', '$product_name', '$product_price', '$product_quantity', '$product_image', 'ordering')") or die('query failed');
         $message[] = 'Sản phẩm được thêm vào giỏ!';
     } else {
-        header('location: login.php');
+        // header('location: login.php');
     }
 }
 ?>
@@ -38,7 +38,6 @@ if (isset($_POST['add_to_cart'])) {
     <link rel="icon" href="img/icon.png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <!-- <script src="js/scripst.js"></script> -->
     <title>Book shop</title>
 </head>
 
