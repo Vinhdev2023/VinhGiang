78566f083108a9206d8b0144bd6fef4c81c9983d
Nguyễn Giang <114151081+kohaiji@users.noreply.github.com>
2023-08-09 20:38:51 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,17 +4,17 @@
 
       <div class="box">
          <h3>Đường dẫn</h3>
-         <a href="index.php">Trang chủ</a>
-         <a href="contact.php">Liên hệ</a>
-         <a href="about.php">Về Chúng tôi</a>
+         <a href="index.php"><i class="fa-solid fa-house"></i>Trang chủ</a>
+         <a href="contact.php"><i class="fa-solid fa-phone"></i>Liên hệ</a>
+         <a href="about.php"><i class="fa-solid fa-person"></i>Về Chúng tôi</a>
       </div>
 
 
       <div class="box">
          <h3>Đường dẫn</h3>
-         <a href="shop.php">Cửa hàng</a>
-         <a href="cart-notify.php">Giỏ hàng</a>
-         <a href="orders.php">Đơn hàng</a>
+         <a href="shop.php"><i class="fa-solid fa-shop"></i>Cửa hàng</a>
+         <a href="cart-notify.php"><i class="fa-solid fa-cart-shopping"></i>Giỏ hàng</a>
+         <a href="orders.php"><i class="fa-solid fa-bag-shopping"></i>Đơn hàng</a>
       </div>
 
       
