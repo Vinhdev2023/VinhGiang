6610e52ae1b53e3e44e45996ee4b1da4c23410a4
Vinhdev2023 <daothanhvinh2004@gmail.com>
2023-08-09 18:32:24 +0700

    vinh updated

--- admin/logout.php
+++ admin/logout.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
-session_unset();
 unset($_SESSION['username']);
 unset($_SESSION['password']);
 
-// session_destroy();
+
 header('location:index.php');
--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if (isset($_POST['add_to_cart'])) {
     <link rel="icon" href="img/icon.png">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <script src="js/scripst.js"></script>
+    <!-- <script src="js/scripst.js"></script> -->
     <title>Book shop</title>
 </head>
 
