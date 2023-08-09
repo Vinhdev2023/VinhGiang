aa12e5c4fe4d9ce78fd267ebb3cabedbdd61fc26
Nguyễn Giang <114151081+kohaiji@users.noreply.github.com>
2023-08-09 20:25:21 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,38 +3,31 @@
    <div class="box-container">
 
       <div class="box">
-         <h3>quick links</h3>
-         <a href="home.php">home</a>
-         <a href="about.php">about</a>
-         <a href="shop.php">shop</a>
-         <a href="contact.php">contact</a>
+         <h3>Đường dẫn</h3>
+         <a href="index.php">Trang chủ</a>
+         <a href="about.php">Chúng tôi</a>
+         <a href="shop.php">Cửa hàng</a>
       </div>
 
 
       <div class="box">
-         <h3>extra links</h3>
-         <a href="login.php">login</a>
-         <a href="register.php">register</a>
-         <a href="cart.php">cart</a>
-         <a href="orders.php">orders</a>
+         <h3>Đường dẫn</h3>
+         <a href="cart-notify.php">Giỏ hàng</a>
+         <a href="orders.php">Đơn hàng</a>
+         <a href="contact.php">Liên hệ</a>
       </div>
 
       
       <div class="box">
-         <h3>contact info</h3>
+         <h3>Thông tin liên hệ</h3>
          <!-- <p> <i class="fas fa-phone"></i> +0123456789 </p> -->
-         <p> <i class="fas fa-phone"></i> +0987654321 </p>
+         <p> <i class="fas fa-phone"></i> +0999999999 </p>
          <p> <i class="fas fa-envelope"></i> ntg1611@gmail.com </p>
+         <p> <i class="fas fa-envelope"></i> daothanhvinh2004@gmail.com </p>
          <p> <i class="fas fa-map-marker-alt"></i> Hanoi - Vietnam </p>
       </div>
 
-      <!-- <div class="box">
-         <h3>follow us</h3>
-         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
-         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
-         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
-         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
-      </div> -->
+   
 
    </div>
 
